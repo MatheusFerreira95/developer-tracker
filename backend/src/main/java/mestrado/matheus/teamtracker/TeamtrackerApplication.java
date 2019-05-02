bfc5f8620d9f480e06a08727b51d79f060d753c0
Matheus Silva Ferreira <matheussf95@gmail.com.br>
2019-05-02 15:57:31 -0300

    iniciando projeto seguindo as diretrizes de https://github.com/jonashackt/spring-boot-vuejs

---
+++ backend/src/main/java/mestrado/matheus/teamtracker/TeamtrackerApplication.java
@@ -0,0 +1,13 @@
+package mestrado.matheus.teamtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mestrado/matheus/teamtracker/TeamtrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package mestrado.matheus.teamtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamtrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
