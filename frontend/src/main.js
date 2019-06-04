eace3b811b66e60249124e23bc9d947c947f6c3e
Matheus Silva Ferreira <matheussf95@gmail.com.br>
2019-06-04 16:07:54 -0300

    gerando novo front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import './plugins/vuetify'
+import App from './App.vue';
+import router from "./router/";
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
