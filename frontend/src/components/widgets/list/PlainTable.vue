e5b4283aab4e76b0f976fd816d47eaa55431f2b9
Matheus Silva Ferreira <matheussf95@gmail.com.br>
2019-06-04 20:34:54 -0300

    melhorando tabela developers

--- frontend/src/components/widgets/list/PlainTable.vue
+++ frontend/src/components/widgets/list/PlainTable.vue
@@ -1,27 +1,40 @@
 <template>
   <v-card>
     <v-toolbar card dense color="transparent">
-      <v-toolbar-title><h4>Developers</h4></v-toolbar-title>
+      <v-toolbar-title>
+        <h4>Developers</h4>
+      </v-toolbar-title>
     </v-toolbar>
     <v-divider></v-divider>
     <v-card-text class="pa-0">
       <template>
         <v-data-table
           :headers="headers"
-          :items="projects"
+          :items="developers"
           hide-actions
           class="elevation-0"
+          :expand="expand"
+          item-key="name"
         >
           <template slot="items" slot-scope="props">
-            <td>
-              <v-avatar size="36px">
-                <img :src="props.item.avatar" :alt="props.item.username" />
-              </v-avatar>
-            </td>
-            <td>{{ props.item.name }}</td>
-            <td class="text-xs-left">{{ props.item.commits }}</td>
-            <td class="text-xs-left">{{ props.item.firstCommit }}</td>
-            <td class="text-xs-left">{{ props.item.lastCommit }}</td>
+            <tr @click="props.expanded = !props.expanded">
+              <td>
+                <v-avatar size="36px">
+                  <img :src="props.item.avatar" :alt="props.item.username">
+                </v-avatar>
+              </td>
+              <td>{{ props.item.name }}</td>
+              <td class="text-xs-left">{{ props.item.commits }}</td>
+              <td class="text-xs-left">{{ props.item.firstCommit }}</td>
+              <td class="text-xs-left">{{ props.item.lastCommit }}</td>
+            </tr>
+          </template>
+          <template v-slot:expand="props">
+            <v-card flat>
+              <v-card-text>Active Days: {{props.item.activeDays}}</v-card-text>
+              <v-card-text>LOC: {{props.item.loc}}</v-card-text>
+              <v-card-text>Commits per type file: {{props.item.fileCountCommits}}</v-card-text>
+            </v-card>
           </template>
         </v-data-table>
       </template>
@@ -31,32 +44,124 @@
 </template>
 
 <script>
-import { Projects } from '@/api/project';
 export default {
-  data () {
+  data() {
     return {
       headers: [
         {
-          text: '',
-          align: 'center',
+          text: "",
+          align: "center",
           sortable: false,
-          value: 'avatar'
+          value: "avatar"
         },
         {
-          text: 'Name',
-          align: 'left',
-          value: 'name'
+          text: "Name",
+          align: "left",
+          value: "name"
         },
-        { text: 'Commits', value: 'commits' },
-        { text: 'First Commit', value: 'firstCommit' },
-        { text: 'Last Commit', value: 'lastCommit' }
+        { text: "Commits", value: "commits" },
+        { text: "First Commit", value: "firstCommit" },
+        { text: "Last Commit", value: "lastCommit" }
       ],
+      developers: [
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Jhon",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Snow",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Jhon Snow",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Jhon Snow",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Dessie Snow",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Dessie",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Jhon Dessie",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        },
+        {
+          commits: "Dessie",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg",
+          firstCommit:
+            "10/10/2017",
+          name: "Ramires Snow",
+          lastCommit: "10/10/2018",
+          activeDays: 10,
+          loc: 90,
+          fileCountCommits: "Java - 10%; JS - 90%",
+        }
+      ]
     };
-  },
-  computed: {
-    projects () {
-      return Projects;
-    }
   }
 };
 </script>
