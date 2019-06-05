abff9715ef1d855fd6daf7e644075343d85553b4
Matheus Silva Ferreira <matheussf95@gmail.com.br>
2019-06-05 16:53:59 -0300

    criando forma de clonar repositorio git com credenciais

--- backend/src/main/java/mestrado/matheus/teamtracker/controller/ProjectController.java
+++ backend/src/main/java/mestrado/matheus/teamtracker/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package mestrado.matheus.teamtracker.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import mestrado.matheus.teamtracker.domain.Filter;
 
@@ -12,20 +14,35 @@ import mestrado.matheus.teamtracker.domain.Filter;
 @RequestMapping("/project")
 public class ProjectController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ProjectController.class);
 
-    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+	@RequestMapping(path = "/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public @ResponseBody String addNewUser(@RequestParam Filter filter) {
 
-    @RequestMapping(path = "/test")
-    public @ResponseBody String sayHello() {
-        LOG.info("GET called on /hello resource");
-        return HELLO_TEXT;
-    }
+		return filter.toString();
+	}
+	
+	/*
+	private static final Logger LOG = LoggerFactory.getLogger(ProjectController.class);
+	public static final String CLONE_WITH_CREDENTIALS = "CLONE WITH CREDENTIALS";
 
-    @RequestMapping(path = "/", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody void addNewUser (@RequestParam Filter filter) {
-        
-        LOG.info(filter.toString());
-    }
+	@RequestMapping(path = "/cloneWithCredentials")
+	public @ResponseBody String cloneWithCredentials() {
+		LOG.warn(CLONE_WITH_CREDENTIALS);
+
+		try {
+			File local = File.createTempFile("criado2", "");
+			Path localPath = Paths.get(System.getProperty("java.io.tmpdir") + "/criado2");
+			Git.gitClone(localPath, "https://USUARIO_AQUI:SENHA_AQUI@github.com/MatheusFerreira95/team-tracker.git");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return CLONE_WITH_CREDENTIALS;
+	}
+*/
 }
---
+++ backend/src/main/java/mestrado/matheus/teamtracker/util/Git.java
@@ -0,0 +1,59 @@
+package mestrado.matheus.teamtracker.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Git {
+
+	public static void gitClone(Path directory, String originUrl) throws IOException, InterruptedException {
+		runCommand(directory.getParent(), "git", "clone", originUrl, directory.getFileName().toString());
+	}
+
+	public static void runCommand(Path directory, String... command) throws IOException, InterruptedException {
+		Objects.requireNonNull(directory, "directory");
+		if (!Files.exists(directory)) {
+			throw new RuntimeException("can't run command in non-existing directory '" + directory + "'");
+		}
+		ProcessBuilder pb = new ProcessBuilder().command(command).directory(directory.toFile());
+		Process p = pb.start();
+		StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
+		StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
+		outputGobbler.start();
+		errorGobbler.start();
+		int exit = p.waitFor();
+		errorGobbler.join();
+		outputGobbler.join();
+		if (exit != 0) {
+			throw new AssertionError(String.format("runCommand returned %d", exit));
+		}
+	}
+
+	private static class StreamGobbler extends Thread {
+
+		private final InputStream is;
+		private final String type;
+
+		private StreamGobbler(InputStream is, String type) {
+			this.is = is;
+			this.type = type;
+		}
+
+		@Override
+		public void run() {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					System.out.println(type + "> " + line);
+				}
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
+
+}
