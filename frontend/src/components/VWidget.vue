233711e1bda62e899340f0e32daffdb19fa52b87
Matheus Silva Ferreira <matheussf95@gmail.com>
2020-06-06 15:44:47 -0300

    adicionando informação sobre interações disponíveis no gráfico de desenvolvedores

--- frontend/src/components/VWidget.vue
+++ frontend/src/components/VWidget.vue
@@ -2,7 +2,7 @@
   <div id="v-widget">
     <v-card>
       <v-toolbar color="transparent" flat dense card v-if="enableHeader">
-        <v-toolbar-title>
+        <v-toolbar-title style="width:100%">
           <h4 v-if="title">{{title}}</h4>
           <div v-else>
             <v-chip outlined label @click="changeLevel('init')">Project/</v-chip>
@@ -15,6 +15,55 @@
               :disabled="index === title2.paths.length - 1"
               @click="changeLevel(index)"
             >{{ index === 0 ? item.descrition : item.name }}/</v-chip>
+            <div class="text-center" style="float:right">
+              <v-menu open-on-hover bottom offset-y>
+                <template v-slot:activator="{ on }">
+                  <v-icon
+                    right
+                    v-on="on"
+                    color="primary"
+                    style="position: absolute; top: -35px; right: -10px;"
+                  >info</v-icon>
+                </template>
+
+                <v-card>
+                  <v-toolbar card dense color="transparent">
+                    <v-toolbar-title>
+                      <h4>Interactions</h4>
+                    </v-toolbar-title>
+                  </v-toolbar>
+                  <v-divider></v-divider>
+                  <v-card-text class="pa-0">
+                    <template>
+                      <span>
+                        <b>&nbsp; - Artifact Zoom In:</b> click on an artifact &nbsp;
+                      </span>
+                      <br />
+                      <span>
+                        <b>&nbsp; - Artifact Zoom Out:</b> click on the zoom history item positioned on the graph toolbar &nbsp;
+                      </span>
+                      <br />
+                      <span>
+                        <b>&nbsp; - Filter:</b> hover an artifact, developer or connection &nbsp;
+                      </span>
+                      <br />
+                      <span>
+                        <b>&nbsp; - Move item:</b> click and drag an artifact or developer &nbsp;
+                      </span>
+                      <br />
+                      <span>
+                        <b>&nbsp; - Move graph:</b> click on a blank part and drag the graph &nbsp;
+                      </span>
+                      <br />
+                      <span>
+                        <b>&nbsp; - Graphical Zoom in/out:</b> use the mouse scroll on the graph &nbsp;
+                      </span>
+                      <br />
+                    </template>
+                  </v-card-text>
+                </v-card>
+              </v-menu>
+            </div>
           </div>
         </v-toolbar-title>
         <v-spacer></v-spacer>
